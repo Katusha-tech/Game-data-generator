94c6622b1e3e13406e7340c69ac786a3c07941cc
Ekaterina <katusha158@rambler.ru>
2026-05-25 23:53:45 +0300

    Com_9: добавлена автоматическая аналитическая витрина user_activity_summary

--- game_etl.py
+++ game_etl.py
@@ -114,7 +114,50 @@ def validate_events(events_list):
         valid_events.append(event)
 
     return valid_events
+def update_user_activity_summary():
+    """Обновляет витрину user_activity_summary"""
 
+    try:
+        conn = psycopg2.connect(**DB_CONFIG)
+
+        with conn.cursor() as cur:
+
+            # очищаем витрину
+            cur.execute("""
+                TRUNCATE TABLE user_activity_summary;
+            """)
+
+            # загружаем агрегаты
+            cur.execute("""
+                INSERT INTO user_activity_summary (
+                    user_id,
+                    total_events,
+                    total_sessions,
+                    total_purchases,
+                    total_ad_views
+                )
+
+                SELECT
+                    user_id,
+                    COUNT(*) AS total_events,
+                    COUNT(DISTINCT session_id) AS total_sessions,
+                    SUM(CASE WHEN event_type = 'purchase' THEN 1 ELSE 0 END) AS total_purchases,
+                    SUM(CASE WHEN event_type = 'ad_view' THEN 1 ELSE 0 END) AS total_ad_views
+
+                FROM raw_events
+                GROUP BY user_id;
+            """)
+
+        conn.commit()
+
+        print("Витрина user_activity_summary обновлена")
+
+    except Exception as e:
+        print(f"Ошибка обновления витрины: {e}")
+
+    finally:
+        if 'conn' in locals():
+            conn.close()
 
 # -----------------------------
 # 5. Основной ETL процесс
@@ -159,7 +202,8 @@ if __name__ == "__main__":
             print(f"Отброшено событий: {dropped}")
 
             # Загрузка в БД и логирование
-            rows_loaded = load_events(events)
+            rows_loaded = load_events(valid_events)
+            update_user_activity_summary()
             log_etl("raw_events", rows_loaded, "SUCCESS", rows_dropped=dropped)
 
         except Exception as e:
